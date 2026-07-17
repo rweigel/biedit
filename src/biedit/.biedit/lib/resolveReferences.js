2002d77c873f57410e835a3ef31ecfb7b26aa87c
Bob Weigel <rweigel@gmu.edu>
2026-07-17 08:31:36 -0400

    refactor

--- src/biedit/.biedit/lib/resolveReferences.js
+++
@@ -1,6 +0,0 @@
-function resolveReferences() {
-  $(".lref").each(function (idx, el) {
-    let href = $(el).attr('href');
-    console.log($(`.lanchor[href="${href}"]`))
-  });
-}
--- src/biedit/__main__.py
+++
@@ -1,4 +0,0 @@
-from biedit._main import main
-
-if __name__ == "__main__":
-    main()
